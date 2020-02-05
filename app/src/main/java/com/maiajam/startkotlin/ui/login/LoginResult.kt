83b2f13fc96631e7eda0e1e88038fca7d0675a22
mai waleed <maiajam88@hotmail.com>
2020-02-05 12:06:10 +0200

    Initial commit

---
+++ app/src/main/java/com/maiajam/startkotlin/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.maiajam.startkotlin.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
